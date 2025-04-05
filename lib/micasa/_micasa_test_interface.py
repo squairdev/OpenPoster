fe6869ce324939c080f4acb677fbc0211f912359
ItMe12sTH <itmeshinnapat@gmail.com>
2025-04-05 23:25:41 +0700

    interface, no functions calling yet.

--- lib/micasa/_micasa_test_interface.py
+++ lib/micasa/_micasa_test_interface.py
@@ -10,6 +10,9 @@ from PySide6.QtWidgets import (
     QLineEdit,
     QPushButton,
     QLabel,
+    QCheckBox,
+    QGroupBox,
+    QHBoxLayout
 )
 from PySide6.QtGui import (
     QIcon,
@@ -36,7 +39,11 @@ class MainWindow(QMainWindow):
             print("skipping icon")
         
         central_widget = QWidget(self)
-        layout = QVBoxLayout()
+        main_layout = QVBoxLayout()
+
+        # for these input and whatever ONLY
+        mx_group = QGroupBox("XML Export Settings")
+        mx_group_layout = QVBoxLayout()
 
         #make_xml(
         #    self,
@@ -50,23 +57,59 @@ class MainWindow(QMainWindow):
         #    withRoot = True
         #):
 
+        # make xml inputs
         self.mxinput_startframe = QLineEdit(self)
         self.mxinput_startframe.setPlaceholderText("Starting frame number")
-        layout.addWidget(self.mxinput_startframe)
+        mx_group_layout.addWidget(self.mxinput_startframe)
 
         self.mxinput_endframe = QLineEdit(self)
         self.mxinput_endframe.setPlaceholderText("Ending frame number")
-        layout.addWidget(self.mxinput_endframe)
+        mx_group_layout.addWidget(self.mxinput_endframe)
 
         self.mxinput_fileprefix = QLineEdit(self)
         self.mxinput_fileprefix.setPlaceholderText("File prefix (e.g. FrameID_ if you have one)")
-        layout.addWidget(self.mxinput_fileprefix)
+        mx_group_layout.addWidget(self.mxinput_fileprefix)
 
         self.mxinput_fileextension = QLineEdit(self)
         self.mxinput_fileextension.setPlaceholderText("File extension (e.g. .png, .jpg)")
-        layout.addWidget(self.mxinput_fileextension)
+        mx_group_layout.addWidget(self.mxinput_fileextension)
 
-        central_widget.setLayout(layout)
+        self.mxinput_padding = QLineEdit(self)
+        self.mxinput_padding.setPlaceholderText("Padding (e.g. 0001 will be 4)")
+        mx_group_layout.addWidget(self.mxinput_padding)
+
+        self.mxinput_fps = QLineEdit(self)
+        self.mxinput_fps.setPlaceholderText("FPS (e.g. 24, 30, 60)")
+        mx_group_layout.addWidget(self.mxinput_fps)
+
+        self.mxinput_exportasanimation = QCheckBox("Export as animation object")
+        mx_group_layout.addWidget(self.mxinput_exportasanimation)
+
+        self.mxinput_exportasanimation = QCheckBox("Include <root> tag")
+        mx_group_layout.addWidget(self.mxinput_exportasanimation)
+
+        # layout first
+        button_layout = QHBoxLayout()
+        button_layout.addStretch()
+
+        # add mx box
+        mx_group.setLayout(mx_group_layout)
+        mx_group.setFixedSize(600, 300)
+        main_layout.addWidget(mx_group, alignment=Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+
+        # make xml commands
+        self.mx_generate_button = QPushButton("Generate", self)
+        self.mx_generate_button.setFixedSize(150, 30)
+        button_layout.addWidget(self.mx_generate_button)
+
+        self.mx_preview_button = QPushButton("Preview", self)
+        self.mx_preview_button.setFixedSize(150, 30)
+        button_layout.addWidget(self.mx_preview_button)
+
+        mx_group_layout.addLayout(button_layout)
+
+        # finalize
+        central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
 
 
