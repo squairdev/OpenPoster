2439516834f7bec1e2a8ce2c604da46f35129458
retronbv <me@retron.dev>
2025-04-05 13:57:00 -0400

    gui kind of working, inspector is not working

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from PySide6 import QtCore, QtWidgets
+import sys
+from gui.placeholder import MainWindow
 
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MainWindow()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
