302d1b9ea3da2b25df800f69bb1b0ef0b872c557
ItMe12sTH <itmeshinnapat@gmail.com>
2025-04-10 00:06:23 +0700

    Interface for picking object and file

--- lib/micasa/_micasa_test_interface.py
+++ lib/micasa/_micasa_test_interface.py
@@ -9,10 +9,10 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QLineEdit,
     QPushButton,
-    QLabel,
     QCheckBox,
     QGroupBox,
-    QHBoxLayout
+    QHBoxLayout,
+    QFileDialog
 )
 from PySide6.QtGui import (
     QIcon,
@@ -23,14 +23,18 @@ import xml.etree.ElementTree as et
 import xml.dom.minidom as minidom
 
 from micasa import XmlGenerator as XmlGen
+from micasa import AnimationObjectEditor as AnimEdit
 
 # app
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Micasa test ui")
-        self.setFixedSize(QSize(800, 600))
+        self.setFixedSize(QSize(600, 480))
         self.XmlGen = XmlGen()
+        self.AnimEdit = AnimEdit()
+        self._object_saved = None
+        self._file_path = None
 
         self.base_path = os.path.dirname(__file__)
         self.icon_path = os.path.abspath(os.path.join(self.base_path, "..", "..", "assets", "openposter.ico"))
@@ -62,7 +66,7 @@ class MainWindow(QMainWindow):
 
         # make xml inputs
         self.mxinput_startframe = QLineEdit(self)
-        self.mxinput_startframe.setPlaceholderText("Starting frame number (Required)")
+        self.mxinput_startframe.setPlaceholderText("Starting frame number (Required e.g. 0 or 1)")
         mx_group_layout.addWidget(self.mxinput_startframe)
 
         self.mxinput_endframe = QLineEdit(self)
@@ -97,28 +101,61 @@ class MainWindow(QMainWindow):
 
         # add mx box
         mx_group.setLayout(mx_group_layout)
-        mx_group.setFixedSize(600, 300)
+        mx_group.setFixedSize(550, 300)
         main_layout.addWidget(mx_group, alignment=Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
 
         # make xml commands
-        #self.mx_generate_button = QPushButton("Generate", self)
-        #self.mx_generate_button.setFixedSize(150, 30)
-        #button_layout.addWidget(self.mx_generate_button)
+        self.mx_generate_button = QPushButton("Save to memory", self)
+        self.mx_generate_button.setFixedSize(150, 30)
+        button_layout.addWidget(self.mx_generate_button)
 
         self.mx_preview_button = QPushButton("Preview", self)
         self.mx_preview_button.setFixedSize(150, 30)
         button_layout.addWidget(self.mx_preview_button)
 
-        self.mx_preview_button.clicked.connect(self.previewXmlData)
+        # GEN/PREVIEW FUNC
+        self.mx_generate_button.clicked.connect(lambda: self.previewXmlData(True))
+        self.mx_preview_button.clicked.connect(lambda: self.previewXmlData(False))
 
         mx_group_layout.addLayout(button_layout)
+
+        # injector menu
+        inject_group = QGroupBox("Direct Inject (Requires export as animation and no root)")
+        inject_group_layout = QVBoxLayout()
+
+        self.inject_targettype = QLineEdit(self)
+        self.inject_targettype.setPlaceholderText("Target type (Required e.g. CALayer)")
+        inject_group_layout.addWidget(self.inject_targettype)
+
+        # attr should be set to "name" automatically
+
+        self.inject_targetname = QLineEdit(self)
+        self.inject_targetname.setPlaceholderText("Target name (Required e.g. MyTargetLayer)")
+        inject_group_layout.addWidget(self.inject_targetname)
+
+        inject_button_layout = QHBoxLayout()
+        inject_button_layout.addStretch()
+
+        self.inject_button = QPushButton("INJECT (Just put the main.caml file in this directory for this demo)", self)
+        self.inject_button.setFixedSize(400, 30)
+        inject_button_layout.addWidget(self.inject_button)
+
+        # INJECT FUNC
+        self.inject_button.clicked.connect(lambda: self.injectObject())
+
+        inject_group_layout.addLayout(inject_button_layout)
+
+        # injector box
+        inject_group.setLayout(inject_group_layout)
+        inject_group.setFixedSize(550, 150)
+        main_layout.addWidget(inject_group, alignment=Qt.AlignmentFlag.AlignCenter)
 
         # finalize
         central_widget.setLayout(main_layout)
         self.setCentralWidget(central_widget)
 
     # checking and stuff
-    def previewXmlData(self):
+    def previewXmlData(self, save_to_memory):
         start_frame = self.mxinput_startframe.text()
         end_frame = self.mxinput_endframe.text()
         file_prefix = self.mxinput_fileprefix.text()
@@ -154,10 +191,34 @@ class MainWindow(QMainWindow):
             fps=fps,
             withRoot=with_root
         )
+
+        if save_to_memory:
+            self._object_saved = data
+            print(save_to_memory)
+            return
         
         xml_str = et.tostring(data.getroot(), 'utf-8')
         formatted = minidom.parseString(xml_str).toprettyxml("\t")
         print(formatted)
+
+    # injecting func call
+    def injectObject(self):
+        type = self.inject_targettype.text()
+        name = self.inject_targetname.text()
+
+        if not type:
+            print("Missing type")
+            return
+        if not name:
+            print("Missing name")
+            return
+        if not self._object_saved:
+            print("no saved object in memory")
+            return
+
+        self.AnimEdit.load_file("main.caml")
+        self.AnimEdit.insert_object_to_target(type, "name", name, self._object_saved.getroot())
+        self.AnimEdit.save_file("main_output.caml")
 
 
 # central loader
